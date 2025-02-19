76734b104c880506810256d25023e0ddc5d9c07a
Junwei Su (PGT) <c4063811@newcastle.ac.uk>
2025-02-19 14:09:32 +0000

    [ADD]添加GTEST

---
+++ tests/test.cpp
@@ -0,0 +1,16 @@
+﻿#include <gtest/gtest.h>
+
+// 一个简单的测试
+TEST(MathTest, Add) {
+    EXPECT_EQ(2 + 2, 4);
+}
+
+TEST(MathTest, Subtract) {
+    EXPECT_EQ(5 - 3, 2);
+}
+
+// main 函数
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
